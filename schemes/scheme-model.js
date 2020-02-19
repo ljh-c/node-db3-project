621bfe8d6e317026ced4c6cfc0b9ccf9f998bc19
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-02-19 15:45:55 -0800

    adds methods add, update, remove

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -7,21 +7,44 @@ function find() {
 function findById(id) {
   return db('schemes').where({ id }).first()
     .then(scheme => {
-      if (scheme === undefined) return null;
-      return scheme;
+      return scheme ? scheme : null;
     });
 }
 
 function findSteps(id) {
-  return db('schemes as sc').
-    join('steps as st', 'sc.id', 'st.scheme_id')
+  return db('schemes as sc')
+    .join('steps as st', 'sc.id', 'st.scheme_id')
     .select('st.id', 'sc.scheme_name', 'st.step_number', 'st.instructions')
     .where({ scheme_id: id })
     .orderBy('step_number');
 }
 
+function add(scheme) {
+  return db('schemes').insert(scheme)
+    .then(([id]) => {
+      return findById(id);
+    });
+}
+
+function update(changes, id) {
+  return db('schemes').where({ id }).update(changes)
+    .then(() => {
+      return findById(id);
+    });
+}
+
+function remove(id) {
+  return findById(id).then(async scheme => {
+    await db('schemes').where({ id }).del();
+    return scheme;
+  });
+}
+
 module.exports = {
   find,
   findById,
-  findSteps
+  findSteps,
+  add,
+  update,
+  remove
 };
